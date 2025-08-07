bfe1a7623a1bee8cd58a90732d802b956ac58a61
정혜윤 <a20897479@mail.com>
2025-08-07 21:02:39 +0900

    패키지제이슨 수정

---
+++ backend/pages/api/portfolio.js
@@ -0,0 +1,7 @@
+// pages/api/portfolio.js
+export default function handler(req, res) {
+    if (req.method === 'GET') {
+        // DB에서 포트폴리오 데이터 조회
+        // res.json({ success: true, data: [...] })
+    }
+}
